1d6e97e8b6692a94a3a8be8a80cd7edea3b71e16
Gaurav Bole <gauravbole94@gmail.com>
2021-11-18 17:58:54 +0530

    Update producer.py

--- producer.py
+++ producer.py
@@ -6,9 +6,13 @@ from data_generator import get_random_data
 def json_serializer(data):
     return json.dumps(data).encode('utf-8')
 
+# send data to perticular partaion
+def get_partitioner(key_bytes, all_partitions, available_partitions):
+    return 1
 
 producer = KafkaProducer(bootstrap_servers=["172.18.0.2:6667"],
-                         value_serializer=json_serializer)
+                         value_serializer=json_serializer,
+                         partitioner=get_partitioner)
 
 if __name__ == "__main__":
     while True:

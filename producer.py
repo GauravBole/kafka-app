0b6f07575a0af8ce6351fdcade7ab87d3385ad53
Gaurav Bole <gauravbole94@gmail.com>
2021-11-18 15:50:32 +0530

    Create producer.py

---
+++ producer.py
@@ -0,0 +1,22 @@
+from kafka import KafkaProducer
+import json
+import time
+from data_generator import get_random_data
+
+def json_serializer(data):
+    return json.dumps(data).encode('utf-8')
+
+
+producer = KafkaProducer(bootstrap_servers=["172.18.0.2:6667"],
+                         value_serializer=json_serializer)
+
+if __name__ == "__main__":
+    while True:
+        random_data = get_random_data()
+        print(random_data)
+        producer.send(topic="randomdata", value=random_data)
+        time.sleep(3)
+
+
+
+

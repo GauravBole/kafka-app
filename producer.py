3028ae5faa9e02e54ed374b4a2ea4432421a7bf8
Gaurav Bole <gauravbole94@gmail.com>
2021-11-22 23:01:28 +0530

    Update producer.py

--- producer.py
+++ producer.py
@@ -4,12 +4,16 @@ import time
 from data_generator import get_random_data
 
 def json_serializer(data):
+    """ serilaize and endcode data """
+    
     return json.dumps(data).encode('utf-8')
 
-# send data to perticular partaion
+
+# send data to specific partition
 def get_partitioner(key_bytes, all_partitions, available_partitions):
     return 1
 
+# partitioner is optional if we want to publish message to perticular partation only.
 producer = KafkaProducer(bootstrap_servers=["172.18.0.2:6667"],
                          value_serializer=json_serializer,
                          partitioner=get_partitioner)
